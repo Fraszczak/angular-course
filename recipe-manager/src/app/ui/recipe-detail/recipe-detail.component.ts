cbb921887cbf5ab286370ef6d224bd867ae82581
Piotr <Piotr.fraszczak@hotmail.com>
2024-11-17 11:03:41 +0100

    new control flow

--- recipe-manager/src/app/ui/recipe-detail/recipe-detail.component.ts
+++ recipe-manager/src/app/ui/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,11 @@
-import { NgIf } from '@angular/common';
+
 import { Component, Input } from '@angular/core';
 import { RecipeModel } from '../../core/recipe/model';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [NgIf],
+  imports: [],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.scss'
 })
