79fc96a47b03c9c8358aa262fbe4d9a27a0e8e7f
Piotr <Piotr.fraszczak@hotmail.com>
2025-01-06 20:18:49 +0100

    no barrel files

--- recipe-manager/src/app/app.component.ts
+++ recipe-manager/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RecipeListComponent } from './ui/recipe-list';
-import { RecipeDetailComponent } from './ui/recipe-detail/';
-import { RecipeModel } from './core/recipe/model';
-import { RecipeTemplateFormComponent } from './ui/recipe-template-form';
 import { MatButtonModule } from '@angular/material/button';
+import { RecipeModel } from './core/recipe/model/recipe.model';
+import { RecipeDetailComponent } from './ui/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './ui/recipe-list/recipe-list.component';
+import { RecipeTemplateFormComponent } from './ui/recipe-template-form/recipe-template-form.component';
+
 
 @Component({
   selector: 'app-root',
